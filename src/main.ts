9db0162ec980946d82d2c228196900751386ab45
Gary Montgomery <54603459+garysm@users.noreply.github.com>
2021-10-25 09:52:31 -0400

    add starter script

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ declare global {
     Example types, expand on these or remove them and add your own.
     Note: Values, properties defined here do no fully *exist* by this type definiton alone.
           You must also give them an implemention if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
-
     Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
     Interfaces matching on name from @types/screeps will be merged. This is how you can extend the 'built-in' interfaces from @types/screeps.
   */
@@ -17,8 +16,8 @@ declare global {
 
   interface CreepMemory {
     role: string;
-    room: string;
-    working: boolean;
+    // room: string;
+    // working: boolean;
   }
 
   // Syntax for adding proprties to `global` (ex "global.log")
@@ -33,6 +32,45 @@ declare global {
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
+
+  const harvesters: Creep[] = _.filter(Game.creeps, (creep) => creep.memory.role == "harvester");
+
+  const spawn1 = Game.spawns["Spawn1"];
+
+  if (harvesters.length < 2) {
+    let name = 'Harvester' + Game.time;
+    let body = [WORK, CARRY, MOVE];
+    let memory = { role: 'harvester' };
+
+    let memoryData = { name, body, memory };
+
+    spawn1.spawnCreep([WORK, CARRY, MOVE, MOVE], `Harvester-${Game.time}`, memoryData);
+  }
+
+  //run for each harvester
+  for (const name in harvesters) {
+    const creep = harvesters[name];
+
+    if (creep.store.getFreeCapacity() > 0) {
+      var sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+      }
+    }
+    else {
+      var targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_SPAWN) &&
+            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+        }
+      });
+      if (targets.length > 0) {
+        if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+        }
+      }
+    }
+  }
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
