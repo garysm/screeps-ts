fbf86da863d2e5d19e14600002fd880405da83b2
garysm <gary@sabiasoftware.com>
2021-10-30 17:16:51 -0400

    add refuelling and advanced harvesters

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ declare global {
     building?: boolean;
     harvesting?: boolean;
     refilling?: boolean;
+    refueling?: boolean;
     repairing?: boolean;
   }
 
@@ -39,7 +40,30 @@ declare global {
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
 
+  // TODO write initilization code for colony
+
   let myRooms = _.filter(Game.rooms, (r) => r.controller && r.controller.my);
+  const spawn1Room = Game.spawns['Spawn1'].room;
+  const spawn1EnergyCapacity = Game.rooms[spawn1Room.name].energyCapacityAvailable;
+  const spawn1EnergyAvailable = Game.rooms[spawn1Room.name].energyAvailable;
+
+  const containers: StructureContainer[] = spawn1Room.find(FIND_STRUCTURES, {
+    filter: (structure) => {
+      return (structure.structureType == STRUCTURE_CONTAINER)
+    }
+  });
+
+  // calculate containers total energy capacity
+  const containerCap = _.sum(containers, c => c.store.getCapacity(RESOURCE_ENERGY));
+
+  // calculate containers total energy available
+  const containerEnergyAvailable = _.sum(containers, c => c.store[RESOURCE_ENERGY]);
+
+  console.log(`Spawn1 Energy Capacity: ${spawn1EnergyCapacity}`);
+  console.log(`Spawn1 Energy Available: ${spawn1EnergyAvailable}`);
+  console.log(`Container Energy Capacity: ${containerCap}`);
+  console.log(`Container Energy Available: ${containerEnergyAvailable}`);
+
   let myCreeps = Game.creeps;
 
   const harvesters: Creep[] = _.filter(myCreeps, (creep) => creep.memory.role == "harvester");
@@ -53,36 +77,33 @@ export const loop = ErrorMapper.wrapLoop(() => {
 
   const spawn1 = Game.spawns["Spawn1"];
 
-  // ensure that there are enough harvesters
-  if (harvesters.length < 6) {
-    spawnHarvesters(spawn1);
-  } else if (repairCreeps.length < 2) {
+  if (harvesters.length < 8) {
+    //Spawn basic harvesters if energy capacity is lower
+    if (spawn1EnergyCapacity < 550 && spawn1EnergyAvailable < 550) {
+      spawnHarvesters(spawn1);
+    } else if (spawn1EnergyCapacity >= 550 && spawn1EnergyAvailable >= 550) {
+      spawnAdvancedHarvesters(spawn1);
+    }
+  }
+  else if (repairCreeps.length < 2) {
     spawnRepairCreeps(spawn1);
-  } else if (builders.length < 4) {
+  }
+  // Only spawn builders if available energy is more than 85% of the container capacity
+  else if (containerEnergyAvailable >= containerCap * 0.85) {
     spawnBuilders(spawn1);
   }
 
-  // if (builders.length < 2) {
-  //   spawnBuilders(spawn1);
-  // }
 
-  // if (repairCreeps.length < 2) {
-  //   spawnRepairCreeps(spawn1);
-  // }
-
-  //run for each harvester
   for (const name in harvesters) {
     const creep = harvesters[name];
     runHarvester(creep);
   }
 
-  //run for each builder
   for (const name in builders) {
     const creep = builders[name];
     runBuilder(creep);
   }
 
-  //run for each repair creep
   for (const name in repairCreeps) {
     const creep = repairCreeps[name];
     runRepairCreep(creep);
@@ -106,46 +127,65 @@ function spawnHarvesters(spawn1: StructureSpawn) {
   spawn1.spawnCreep([WORK, WORK, CARRY, MOVE], `Harvester-${Game.time}`, memoryData);
 }
 
+function spawnAdvancedHarvesters(spawn1: StructureSpawn) {
+  let name = 'Harvester' + Game.time;
+  let body = [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE];
+  let memory = { role: 'harvester', room: spawn1.room.name, harvesting: true };
+
+  let memoryData = { name, body, memory };
+
+  spawn1.spawnCreep([WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE], `Advanced Harvester-${Game.time}`, memoryData);
+}
+
 // spawn builders
 function spawnBuilders(spawn1: StructureSpawn) {
   let name = 'Builder' + Game.time;
   let body = [WORK, CARRY, MOVE];
-  let memory = { role: 'builder', room: spawn1.room.name, harvesting: true };
+  let memory = { role: 'builder', room: spawn1.room.name, refueling: true };
 
   let memoryData = { name, body, memory };
 
-  spawn1.spawnCreep([WORK, WORK, CARRY, CARRY, MOVE], `Builder-${Game.time}`, memoryData);
+  spawn1.spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE], `Builder-${Game.time}`, memoryData);
 }
 
 // spawn repair creeps
 function spawnRepairCreeps(spawn1: StructureSpawn) {
   let name = 'Repair' + Game.time;
   let body = [WORK, CARRY, MOVE];
-  let memory = { role: 'repair', room: spawn1.room.name, harvesting: true };
+  let memory = { role: 'repair', room: spawn1.room.name, refuelling: true };
 
   let memoryData = { name, body, memory };
 
-  spawn1.spawnCreep([WORK, WORK, CARRY, CARRY, MOVE], `Repair-${Game.time}`, memoryData);
+  spawn1.spawnCreep([WORK, CARRY, CARRY, MOVE], `Repair-${Game.time}`, memoryData);
 }
 
+// TODO only deposit to containers if spawn and extension are full
 function runHarvester(creep: Creep) {
-  var refillTargets = creep.room.find(FIND_STRUCTURES, {
+  var spawnRefillTargets = creep.room.find(FIND_STRUCTURES, {
     filter: (structure) => {
-      return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTAINER) &&
+      return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
     },
   });
+
+  var stationTargets = creep.room.find(FIND_STRUCTURES, {
+    filter: (structure) => {
+      return (structure.structureType == STRUCTURE_CONTAINER) &&
+        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+    },
+  });
+
   var constructionTargets = creep.room.find(FIND_MY_CONSTRUCTION_SITES, {
     filter: (structure) => {
       return structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN;
     }
   });
 
-  let availableTaskExists = (refillTargets.length > 0 || constructionTargets.length > 0 || creep.store[RESOURCE_ENERGY] == 0);
+  let availableTaskExists = (spawnRefillTargets.length > 0 || stationTargets.length > 0 || constructionTargets.length > 0 || creep.store[RESOURCE_ENERGY] == 0);
 
   if ((creep.memory.harvesting || availableTaskExists) && creep.store.getFreeCapacity() == 0) {
     creep.memory.harvesting = false;
-    if (refillTargets.length > 0) {
+    if (spawnRefillTargets.length > 0 || stationTargets.length > 0) {
       creep.memory.refilling = true;
     } else {
       if (constructionTargets.length > 0) {
@@ -157,6 +197,7 @@ function runHarvester(creep: Creep) {
   if ((creep.memory.refilling || creep.memory.building) && creep.store[RESOURCE_ENERGY] == 0) {
     creep.memory.refilling = false;
     creep.memory.building = false;
+    creep.memory.upgrading = false;
     creep.memory.harvesting = true;
   }
 
@@ -175,12 +216,20 @@ function runHarvester(creep: Creep) {
     }
 
     else if (creep.memory.refilling) {
-      if (refillTargets.length > 0) {
-        creep.say('🔄 refill');
-        if (creep.transfer(refillTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(refillTargets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+      creep.say('🔄 refill');
+
+      if (spawnRefillTargets.length > 0) {
+        if (creep.transfer(spawnRefillTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(spawnRefillTargets[0], { visualizePathStyle: { stroke: '#ffffff' } });
         }
-      } else {
+      }
+      else if (stationTargets.length > 0) {
+        // refill stations
+        if (creep.transfer(stationTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(stationTargets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+        }
+      }
+      else {
         if (constructionTargets.length > 0 && creep.store[RESOURCE_ENERGY] > 0) {
           creep.memory.refilling = false;
           creep.memory.building = true;
@@ -227,8 +276,8 @@ function runBuilder(creep: Creep) {
 
   // TODO make an init function to assign an initial task
 
-  if ((creep.memory.harvesting || availableTaskExists) && creep.store.getFreeCapacity() == 0) {
-    creep.memory.harvesting = false;
+  if ((creep.memory.refueling || availableTaskExists) && creep.store.getFreeCapacity() == 0) {
+    creep.memory.refueling = false;
     if (constructionTargets.length > 0) {
       //print out the construction targets
       creep.memory.building = true;
@@ -243,27 +292,38 @@ function runBuilder(creep: Creep) {
   if ((creep.memory.building || creep.memory.upgrading) && creep.store[RESOURCE_ENERGY] == 0) {
     creep.memory.building = false;
     creep.memory.upgrading = false;
-    creep.memory.harvesting = true;
+    creep.memory.refueling = true;
   }
 
-  let hasTask = (creep.memory.harvesting || creep.memory.building || creep.memory.upgrading) ? true : false;
+  let hasTask = (creep.memory.refueling || creep.memory.building || creep.memory.upgrading) ? true : false;
 
   if (hasTask) {
-    if (creep.memory.harvesting) {
-      creep.say('⛏ harvest');
-      var sources = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-      if (sources != null) {
-        if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources, { visualizePathStyle: { stroke: '#ffaa00' } });
+    if (creep.memory.refueling) {
+      var stations = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          // Only fill at stations that have some energy
+          return (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0;
         }
+      });
+      if (stations.length > 0) {
+        creep.say('⛽ refuel');
+
+        if (creep.withdraw(stations[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(stations[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+        }
+      } else {
+        rally(creep);
       }
     }
     else if (creep.memory.building) {
       creep.say('🚧 build');
-      if (constructionTargets.length) {
+      if (constructionTargets.length > 0) {
         if (creep.build(constructionTargets[0]) == ERR_NOT_IN_RANGE) {
           creep.moveTo(constructionTargets[0], { visualizePathStyle: { stroke: '#ffffff' } });
         }
+      } else {
+        creep.memory.building = false;
+        rally(creep);
       }
     } else if (creep.memory.upgrading) {
       creep.say('⚡ upgrade');
@@ -273,22 +333,35 @@ function runBuilder(creep: Creep) {
     }
   }
   else {
-    rally(creep);
+    // Only refuel again if we have no task
+    if (creep.store.getFreeCapacity() > 0) {
+      creep.memory.refueling = true;
+    } else {
+      rally(creep);
+    }
   }
 }
 
+const maxWallHits = 10000;
+
 function runRepairCreep(creep: Creep) {
-  var repairTargets = creep.room.find(FIND_STRUCTURES, {
+  var wallTargets = creep.room.find(FIND_STRUCTURES, {
     filter: (structure) => {
-      return structure.structureType == STRUCTURE_WALL
+      return structure.structureType == STRUCTURE_WALL;
     }
   });
 
-  let availableTaskExists = (repairTargets.length > 0 || creep.store[RESOURCE_ENERGY] == 0);
+  var roadTargets = creep.room.find(FIND_STRUCTURES, {
+    filter: (structure) => {
+      return structure.structureType == STRUCTURE_ROAD;
+    }
+  });
 
-  if ((creep.memory.harvesting || availableTaskExists) && creep.store.getFreeCapacity() == 0) {
-    creep.memory.harvesting = false;
-    if (repairTargets.length > 0) {
+  let availableTaskExists = ((wallTargets.length > 0 || roadTargets.length > 0) || creep.store[RESOURCE_ENERGY] == 0);
+
+  if ((creep.memory.refueling || availableTaskExists) && creep.store.getFreeCapacity() == 0) {
+    creep.memory.refueling = false;
+    if (wallTargets.length > 0) {
       creep.memory.repairing = true;
     } else {
       creep.memory.repairing = false;
@@ -297,33 +370,48 @@ function runRepairCreep(creep: Creep) {
 
   if ((creep.memory.repairing) && creep.store[RESOURCE_ENERGY] == 0) {
     creep.memory.repairing = false;
-    creep.memory.harvesting = true;
+    creep.memory.refueling = true;
   }
 
-  let hasTask = (creep.memory.harvesting || creep.memory.repairing) ? true : false;
+  let hasTask = (creep.memory.refueling || creep.memory.repairing) ? true : false;
 
   if (hasTask) {
-    if (creep.memory.harvesting) {
-      creep.say('⛏ harvest');
-      var sources = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-      if (sources != null) {
-        if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources, { visualizePathStyle: { stroke: '#ffaa00' } });
+    if (creep.memory.refueling) {
+      creep.say('⛽ refuel');
+      var stations = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER;
+        }
+      });
+      if (stations.length > 0) {
+        if (creep.withdraw(stations[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(stations[0], { visualizePathStyle: { stroke: '#ffaa00' } });
         }
       }
     }
     else if (creep.memory.repairing) {
-      if (repairTargets.length > 0) {
-        for (let i = 0; i < repairTargets.length; i++) {
-          var wall = repairTargets[i] as StructureWall;
-          if (wall.hits < 1000) {
-            if (creep.repair(repairTargets[i]) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(repairTargets[i]);
+      if (wallTargets.length > 0) {
+        for (let i = 0; i < wallTargets.length; i++) {
+          var wall = wallTargets[i] as StructureWall;
+          if (wall.hits < maxWallHits) {
+            if (creep.repair(wallTargets[i]) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(wallTargets[i]);
+            }
+          }
+        }
+      } else if (roadTargets.length > 0) {
+        for (let i = 0; i < roadTargets.length; i++) {
+          var road = roadTargets[i] as StructureRoad;
+          if (road.hits < road.hitsMax) {
+            if (creep.repair(roadTargets[i]) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(roadTargets[i]);
             }
           }
         }
       }
     }
+  } else {
+    rally(creep);
   }
 }
 
